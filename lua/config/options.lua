c33d4a7fb2457a4aea1de50cd634c03871f3d7f4
James Francis Malquerido <contact@jamesfm.dev>
2023-11-20 12:16:00 +0000

    change vim opts tab sizing space to 4

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,5 @@ opt.shell = "/bin/zsh"
 opt.relativenumber = false
 opt.guicursor =
     "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+opt.shiftwidth = 4
+opt.tabstop = 4
